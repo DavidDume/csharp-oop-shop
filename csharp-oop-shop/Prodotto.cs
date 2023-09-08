ca25c2529d231d9b2130d87b015f3035e4206011
David Dume <dumedavid11@gmail.com>
2023-09-08 15:57:36 +0200

    Add project files.

---
+++ csharp-oop-shop/Prodotto.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_oop_shop
+{
+    
+    public class Prodotto
+    {
+        private Random rnd = new Random();
+        private int codice { get; }
+        public string nome { get; set; }
+        public string descrizione { get; set; }
+        public float prezzo { get; set; }
+        public int iva { get; set; } = 22;
+        public Prodotto(string _nome, string _descrizione, float _prezzo) { 
+            this.codice = rnd.Next();
+            this.nome = _nome;
+            this.descrizione = _descrizione;
+            this.prezzo = _prezzo;
+        }
+        
+        public float GetPriceVAT()
+        {
+            return this.prezzo + ((this.prezzo / 100) * this.iva);
+        }
+
+        public string GetFull()
+        {
+            return this.codice.ToString()+ " " + this.nome;
+        }
+    }
+}
---
+++ csharp-oop-shop/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using csharp_oop_shop;
+
+Prodotto item = new Prodotto("Libro", "Lorem Ipsum", 9.99f);
+
+
+Console.WriteLine(item.GetFull());
+Console.WriteLine(item.GetPriceVAT());
